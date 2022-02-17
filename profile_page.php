eec0d3851224c3788459cadd196494ede16e679e
m17832wa <william.asbery@student.manchester.ac.uk>
2022-02-17 09:08:52 +0000

    sorted the layout and started adding forums

--- profile_page.php
+++ profile_page.php
@@ -1,3 +1,8 @@
+<!-- 
+The profile page of our first year group project from tutorial group Z7
+Coded by: Amy Leigh-Hyer, Daniel Dobzinski, Euan Liew, Frenciel Anggi, Sarah Almuhaythif, Will Asbery, and Yuyao Chen 
+!-->
+
 <?php
 
 session_start();
@@ -107,13 +112,13 @@ if (!isset($_SESSION["logged_in"])) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>linkuni</title>
-	<link rel="stylesheet" type="text/css" href="styling.css">
-	<script type="text/javascript" src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="styling/styling.css">
+	<script type="text/javascript" src="js/script.js"></script>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
