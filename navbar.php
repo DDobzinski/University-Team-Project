7b2441ea1d55cda7bb39df15784f48b66ba31243
v65391yc <yuyao.chen@student.manchester.ac.uk>
2022-02-28 17:45:36 +0000

    a navbar demo

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Navbar demo</title>
+    <link rel="stylesheet" type="text/css" href="styling/navbar.css">
+</head>
+<body>
+    <ul>
+        <li><a href="">Log out</a></li>
+        <li><a href="">Chatroom</a></li>
+        <li><a href="">Profile</a></li>
+        <li><a href="">Homepage</a></li>
+        <li style="float:left"><a class="active" href="">linkuni</a></li>
+    </ul>
+</body>
+</html>
