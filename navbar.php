fe56ecf50d486e7ca58b4a0f0b35a1a8a4420ea1
v65391yc <yuyao.chen@student.manchester.ac.uk>
2022-03-04 10:39:28 +0000

    navbar with logo and issue

--- navbar.php
+++ navbar.php
@@ -6,12 +6,17 @@
     <link rel="stylesheet" type="text/css" href="styling/navbar.css">
 </head>
 <body>
-    <ul>
-        <li><a href="">Log out</a></li>
-        <li><a href="">Chatroom</a></li>
-        <li><a href="">Profile</a></li>
-        <li><a href="">Homepage</a></li>
-        <li style="float:left"><a class="active" href="">linkuni</a></li>
-    </ul>
+    <div id="main">
+        <div id="navbar">
+            <ul>
+
+                <li style="float:left"><a class="active" href=""><img class="logo_image" src="images/linkuni_logo_white.png" alt="Logo image" width="20" height="20"></a></li>
+                <li style="float:right"><a class="active" href="">Log out</a></li>
+                <li style="float:right"><a class="active" href="">Chatroom</a></li>
+                <li style="float:right"><a class="active" href="">Profile</a></li>
+                <li style="float:right"><a class="active" href="">Homepage</a></li>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
