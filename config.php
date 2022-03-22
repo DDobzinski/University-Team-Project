7d6e51e7d63648f2934e1fc842a73f2807e70026
Euan <euan.liew@ac.student.manchester.uk>
2022-03-22 13:27:35 +0000

    Z index of taskbar

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "localhost"; // change when using ;
-$username_db = "master";
-$password = "root";
+$username_db = "newuser";
+$password = "password";
 $db_name = "2021_comp10120_z7";
 
 ?>
