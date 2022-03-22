8c39d57a6e17b6c9daeafca04222a5f3898b12f1
William Asbery <william.asbery@student.manchester.ac.uk>
2022-03-22 13:27:49 +0000

    testing on uni webserver

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "localhost"; // change when using ;
-$username_db = "master";
-$password = "root";
-$db_name = "2021_comp10120_z7";
+$host = "dbhost.cs.man.ac.uk"; // change when using ;
+$username_db = "m17832wa";
+$password = "Zulu7Tutorial";
+$db_name = "m17832wa";
 
 ?>
